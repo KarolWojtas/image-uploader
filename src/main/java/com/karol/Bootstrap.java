b1e88083cf9fb50cc6c50298c3b11718dcc3a253
KarolWojtas <karol.wojtas.aim@gmail.com>
2018-07-28 22:49:42 +0200

    added properties for admin username and password

--- src/main/java/com/karol/Bootstrap.java
+++ src/main/java/com/karol/Bootstrap.java
@@ -1,7 +1,9 @@
 package com.karol;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import com.karol.security.util.CustomUserDetails;
@@ -9,18 +11,25 @@ import com.karol.services.UserDetailsRepository;
 @Component
 public class Bootstrap implements CommandLineRunner{
 	private UserDetailsRepository repository;
+	private PasswordEncoder passwordEncoder;
 	@Autowired
-	public Bootstrap(UserDetailsRepository repository) {
+	public Bootstrap(UserDetailsRepository repository, PasswordEncoder passwordEncoder) {
 		super();
 		this.repository = repository;
-	}	
+		this.passwordEncoder = passwordEncoder;
+	}
+	@Value("${admin.username}")
+	private String username;
+	
+	@Value("${admin.password}")
+	private String password;
 	@Override
 	public void run(String... args) throws Exception {
 		CustomUserDetails user = new CustomUserDetails();
 		user.setFirstName("Karol");
 		user.setLastName("Wojtas");
-		user.setUsername("karolw");
-		user.setPassword("$2a$10$8CzkQVbmyuRoHGkPLKPGYuZV4Y9HgoTYCi5mNupWsfZRRYDsbk7wi");
+		user.setUsername(username);
+		user.setPassword(passwordEncoder.encode(password));
 		user.setRole("ROLE_USER");
 		user.setEmail("k@gmail.com");
 		repository.save(user);
